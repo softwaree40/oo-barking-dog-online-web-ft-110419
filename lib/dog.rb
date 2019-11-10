4b770187b9a47f508086bbfa6dc717af02c0858a
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-10 06:10:43 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+  def name=(name)
+     @my_dog = name 
+   end
+   def name 
+      @my_dog 
+   end
+   def bark
+      puts "woof!"
+     
+   end
+end
+fido = Dog.new
+fido.name = "Fido"
+fido.name
